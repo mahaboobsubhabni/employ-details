96c3d25812d84435435943c7f5d00f4115bb0330
mahaboobsubhabni <112387199+mahaboobsubhabni@users.noreply.github.com>
2022-08-30 18:14:54 +0530

    Add files via upload

---
+++ employ details/employe details.py
@@ -0,0 +1,8 @@
+code=int(input("enter the code"))
+name=input("enter the name")
+company=input("enter the company")
+desc=input("enter the desc")
+salary=str(input("enter the salary"))
+
+print('code /t name /t company /t desc /t salary /t')
+print(code,'/t',name,'/t',company,'/t',desc,'/t',salary)
